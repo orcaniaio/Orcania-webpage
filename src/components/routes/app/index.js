bdaa4bea64c84ed7240441ed8c85bea647b5acb9
loque <madridx2010@gmail.com>
2021-09-28 09:47:58 -0500

    test commit

--- src/components/routes/app/index.js
+++ src/components/routes/app/index.js
@@ -1,7 +1,7 @@
 import {useEffect} from 'react';
 import Sidebar from '../../base/sidebar';
 import Background from '../../base/background';
-import {Link} from  'react-router-dom'; 
+import {Link} from  'react-router-dom';
 
 const BridgeApp = props => {
 
@@ -13,28 +13,25 @@ const BridgeApp = props => {
 
     return (
         <div>
-        <Sidebar/>
-                    <section className="has-background-adanger" style={{height: '100%'}}>
+            <Sidebar/>
+            <section className="has-background-adanger" style={{height: '100%'}}>
 
-                        <div className="main1" style={{width: '100%', height: '100%'}}>
+                <div className="main1" style={{width: '100%', height: '100%'}}>
 
-                            <div className="" style={{position: 'absolute', top: '50%', width: '100%'}}>
-                                <div className="" >
-                                    <div className="" style={{position: 'absolute', left: 'calc(50% - 30px)'}}>
-                                        <div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-                                    </div>
-                                    <div style={{height: '80px'}}></div>
-                                    <div className="title is-2 has-text-centered has-text-white" >Coming soon...</div>
-                                    <div className="subtitle is-5 has-text-centered"><Link to="/" className=" has-text-white">Go back</Link></div>
-                                </div>
-
-
+                    <div className="" style={{position: 'absolute', top: '50%', width: '100%'}}>
+                        <div className="" >
+                            <div className="" style={{position: 'absolute', left: 'calc(50% - 30px)'}}>
+                                <div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
                             </div>
+                            <div style={{height: '80px'}}></div>
+                            <div className="title is-2 has-text-centered has-text-white" >Coming soon...</div>
+                            <div className="subtitle is-5 has-text-centered"><Link to="/" className=" has-text-white">Go back</Link></div>
                         </div>
-                    </section>
-                    <Background/>
-
                     </div>
+                </div>
+            </section>
+            <Background/>
+        </div>
     );
 }
 
