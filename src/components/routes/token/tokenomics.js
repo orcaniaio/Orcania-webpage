1be9b90cb3a96e4d4afffc5b9a31a9b7b64af787
loque <madridx2010@gmail.com>
2021-10-26 12:52:55 -0500

    updated tokenomics

--- src/components/routes/token/tokenomics.js
+++ src/components/routes/token/tokenomics.js
@@ -4,13 +4,6 @@ import './chart.scss';
 
 const tokenomicsData = new Map();
 
-tokenomicsData.set('ICO', {
-    title: 'ICO',
-    body: 'The ICO will be held after the nodes presale has been concluded. </br></br>' +
-    'Any CFC remaining from the nodes presale will be added to the public ICO, it will allow everyone to aquire CFC on multiple blockchains. <br/></br>' +
-    'The ICO will either be held on centralized services such as Binance with part of the ICO being held on DAPPs on the supported blockchains of the Crypto Family Bridge, or it will be fully held on DAPPs of the supported blockchains.'
-});
-
 tokenomicsData.set('Nodes presale', {
     title: 'Nodes presale',
     body: 'The nodes presale is to help early node operators aquire CFC to run validator nodes and secure the Crypto Family Bridge on mainnet release. </br></br>' +
@@ -18,15 +11,15 @@ tokenomicsData.set('Nodes presale', {
     'To join the nodes presale, a user has to have been an early node operator for the Crypto Family Bridge on the testnet release (Soon).'
 });
 
-tokenomicsData.set('Crypto Family nodes', {
-    title: 'Crypto Family nodes',
-    body: 'The Crypto Family is gonna be running the first nodes of the Crypto Family Bridge, and to ensure a secure launch we have allocated 5% of the CFC release supply to these nodes.</br></br>' +
-    'These CFC will forever be allocated to running Crypto Family validator nodes on the bridge, never to be sold or even transfered.</br></br>' +
-    'Remaining CFC from the ICO might also be used to operate more nodes if deemed necessary.'
+tokenomicsData.set('Public sale', {
+    title: 'Public Sale',
+    body: 'The ICO will be held after the nodes presale has been concluded. </br></br>' +
+    'Any CFC remaining from the nodes presale will be added to the public ICO, it will allow everyone to aquire CFC on multiple blockchains. <br/></br>' +
+    'The ICO will either be held on centralized services such as Binance with part of the ICO being held on DAPPs on the supported blockchains of the Crypto Family Bridge, or it will be fully held on DAPPs of the supported blockchains.'
 });
 
-tokenomicsData.set('Crypto Family', {
-    title: 'Crypto Family',
+tokenomicsData.set('Team allocation', {
+    title: 'Team Allocation',
     body: 'The Crypto Family has been allocated 1% of the CFC release supply to distribute to its partners, advisors, contributors, and team allocation.</br></br>' +
     'In reality, to acquire more CFC the Crypto Family team will be participating in the ICO like any other community member, giving the Crypto Family no upper hand over CFC or the Crypto Family Bridge and ensuring these projects are fully decentralized and community driven, and that the Crypto Family is but a part of that community</br></br>'
 });
@@ -37,7 +30,17 @@ tokenomicsData.set('Hackathon', {
     '1% of the CFC release supply will be allocated to help fund such projects and push them forward on their journey, live development support from our team to the teams of these projects will be available to help them better grasp the architecture of the bridge and how to interact with it.</br></br>'
 });
 
+tokenomicsData.set('Crypto family node', {
+    title: 'Crypto Family Node',
+    body: 'The Crypto Family is gonna be running the first nodes of the Crypto Family Bridge, and to ensure a secure launch we have allocated 5% of the CFC release supply to these nodes.</br></br>' +
+    'These CFC will forever be allocated to running Crypto Family validator nodes on the bridge, never to be sold or even transfered.</br></br>' +
+    'Remaining CFC from the ICO might also be used to operate more nodes if deemed necessary.'
+});
 
+tokenomicsData.set('Delegation to nodes', {
+    title: 'Delegation To Nodes ',
+    body: 'lorem ipsum'
+});
 
 const Tokenomics = props => {
 
@@ -61,7 +64,7 @@ const Tokenomics = props => {
     );
 
     const {showData, setShowData} = props;
-    const series = [50, 43, 5, 1, 1];
+    const series = [40, 35, 15, 5, 2.5, 2.5];
 
     const options = {
         chart: {
@@ -72,7 +75,10 @@ const Tokenomics = props => {
                 },
                 dataPointSelection: (event, chartContext, config) => {
 
+                    console.log(config.dataPointIndex);
                     const data = tokenomicsData.get(config.w.globals.labels[config.dataPointIndex]);
+
+                 
 
                     setShowData({
                         body: data.body,
@@ -80,7 +86,8 @@ const Tokenomics = props => {
                     });
                 },
                 dataPointMouseEnter: function(event, chartContext, config) {
-                    // console.log('enter');
+                    console.log( config.w.globals.labels[config.dataPointIndex] );
+
                     const data = tokenomicsData.get(config.w.globals.labels[config.dataPointIndex]);
 
                     setShowData({
@@ -100,11 +107,11 @@ const Tokenomics = props => {
         stroke: {
             colors: ["#DEDEDE"]
         },
-        labels: ['ICO', 'Nodes presale', 'Crypto Family nodes', 'Crypto Family', 'Hackathon'],
+        labels: ['Nodes presale', 'Public sale', 'Team allocation', 'Hackathon', 'Crypto family node', 'Delegation to nodes'],
         fill: {
             opacity: 0.8
         },
-        colors: ['#FF775F', '#23DCF5', '#8A3FFC', '#16C784', '#3861FB'],
+        colors: ['#FF775F', '#23DCF5', '#8A3FFC', '#16C784', '#3861FB', '#ffe121'],
         tooltip: {
             enabled: true,
             followCursor: false,
