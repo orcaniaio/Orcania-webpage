95b64b5c792eeae789bcad43641c63678b1f71af
loque <madridx2010@gmail.com>
2021-10-28 17:36:04 -0500

    table updated

--- src/components/routes/bridge/table.js
+++ src/components/routes/bridge/table.js
@@ -7,49 +7,55 @@ const Table = () => {
                 <table className="table has-background-traansparent has-text-dark is-size-5">
                     <thead>
                         <tr>
-                            <th></th>
-                            <th><h1 className="subtitle is-4 has-text-contrast"> Crypto Family Multichain</h1></th>
+                            <th></th>                            
                             <th><h1 className="subtitle is-4"> Ethereum</h1></th>
+                            <th><h1 className="subtitle is-4"> Polygon</h1></th>
                             <th><h1 className="subtitle is-4"> BSC</h1></th>
                             <th><h1 className="subtitle is-4"> Harmony </h1></th>                            
+                            <th><h1 className="subtitle is-4 has-text-contrast"> Crypto Family Bridge</h1></th>
                         </tr>
                     </thead>
                     <tbody>
                                             
                         <tr>
-                            <th className="has-text-right">TPS</th>
-                            <td className="has-text-contrast has-background-semi">60,000</td>
+                            <th className="has-text-right">TPS</th>                            
                             <td>15</td>
+                            <td>80</td>
                             <td>180</td>
-                            <td>60,000</td>                            
+                            <td>60,000</td>
+                            <td className="has-text-contrast has-background-semi">60,000</td>    
                         </tr>
                         <tr>
-                            <th className="has-text-right">Validators</th>
-                            <td className="has-text-contrast has-background-semi">450</td>
+                            <th className="has-text-right">Validators</th>                            
                             <td>>7500</td>
+                            <td>100</td>
                             <td>21</td>
-                            <td>1000</td>                            
+                            <td>1000</td>
+                            <td className="has-text-contrast has-background-semi">450</td>
                         </tr>
                         <tr>
-                            <th className="has-text-right">Tx Fees</th>
-                            <td className="has-text-contrast has-background-semi">$0.000·0084</td>
+                            <th className="has-text-right">Tx Fees</th>                            
                             <td>$20</td>
+                            <td>$0.00128</td>
                             <td>$0.05</td>
-                            <td>$0.000·0084</td>                            
+                            <td>$0.000·0084</td>
+                            <td className="has-text-contrast has-background-semi">$0.000·0084</td>     
                         </tr>                        
                         <tr>
-                            <th className="has-text-right">Annual Issuance</th>
-                            <td className="has-text-contrast has-background-semi">0-5%</td>
+                            <th className="has-text-right">Annual Issuance</th>                            
                             <td>1%</td>
-                            <td>0%</td>
-                            <td>3%</td>                            
+                            <td>N/A</td>
+                            <td>N/A</td>
+                            <td>3%</td>
+                            <td className="has-text-contrast has-background-semi">0-5%</td>
                         </tr>
                         <tr>
                             <th className="has-text-right">Sharding</th>
-                            <td className="has-text-contrast has-background-semi">15 Blockchains</td>
-                            <td>1 blockchain</td>
-                            <td>1 blockchain</td>
-                            <td>4 shards</td>                            
+                            <td>1 shard</td>
+                            <td>1 shard</td>
+                            <td>1 shard</td>
+                            <td>4 shards</td>
+                            <td className="has-text-contrast has-background-semi">15 shards</td> 
                         </tr>
                     </tbody>
                 </table>
