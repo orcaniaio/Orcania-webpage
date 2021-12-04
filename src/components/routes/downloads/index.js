4214294968f4c2f359adab3cc619c01935ff6613
loque <madridx2010@gmail.com>
2021-12-04 11:05:43 -0500

    add download links

--- src/components/routes/downloads/index.js
+++ src/components/routes/downloads/index.js
@@ -25,26 +25,26 @@ const BridgeApp = props => {
                         <h1 className="subtitle has-text-light-purple is-3 mb-0">Bridge</h1>
                         <h1 className="subtitle has-text-white">Version 1.0.0</h1>
 
-                        <p className="has-text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit turpis cursus in hac habitasse. Nunc pulvinar sapien et ligula.</p>
+                        <p className="has-text-white">Bridge validator nodes</p>
                         <br/>
 
                         <div className="columns is-multiline">
                             {
-                                requirementsData.map( (item, i) => 
-                                    <div className="column is-6" key={i}>
-                                        <div className="media">
-                                            <div className="media-left">
-                                                <figure className="image is-32x32">
-                                                    {item.icon}
-                                                </figure>                                                
-                                            </div>
-                                            <div className="media-content">
-                                                <h1 className="subtitle has-text-white mb-0 has-text-weight-bold">{item.title}</h1>
-                                                <p className="has-text-white">{item.description}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
+                                // requirementsData.map( (item, i) => 
+                                //     <div className="column is-6" key={i}>
+                                //         <div className="media">
+                                //             <div className="media-left">
+                                //                 <figure className="image is-32x32">
+                                //                     {item.icon}
+                                //                 </figure>                                                
+                                //             </div>
+                                //             <div className="media-content">
+                                //                 <h1 className="subtitle has-text-white mb-0 has-text-weight-bold">{item.title}</h1>
+                                //                 <p className="has-text-white">{item.description}</p>
+                                //             </div>
+                                //         </div>
+                                //     </div>
+                                // )
                             }                            
                         </div>
 
@@ -52,16 +52,16 @@ const BridgeApp = props => {
 
                         <div className="columns is-multiline">
                             <div className="column is-6">
-                                <button className="button is-outlined is-fullwidth has-text-light-purple py-5 download-btn">CF Bridge BSC Validator Node </button>
+                                <a className="button is-outlined is-fullwidth has-text-light-purple py-5 download-btn" href="https://drive.google.com/file/d/1CmllHSKKDa_jc7MkCHoYNcYhjSMP4e5H/view?usp=sharing" target="_blank">CF Bridge BSC Validator Node </a>
                             </div>
                             <div className="column is-6">
-                                <button className="button is-fullwidth has-text-light-purple py-5 download-btn">CF Bridge Ethereum Validator Node</button>
+                                <a className="button is-fullwidth has-text-light-purple py-5 download-btn" href="https://drive.google.com/file/d/1xTLrHCqgXYIzQW_um31R_fIX6dJzIghB/view?usp=sharing" target="_blank">CF Bridge Ethereum Validator Node</a>
                             </div>
                             <div className="column is-6">
-                                <button className="button is-fullwidth has-text-light-purple py-5 download-btn">CF Bridge Harmony Validator Node <br/> Coming Soon </button>
+                                <a className="button is-fullwidth has-text-light-purple py-5 download-btn" href="https://drive.google.com/file/d/1iEw41bzy5_ScpyLWNjKj-Df7o31n0zRc/view?usp=sharing" target="_blank">CF Bridge Harmony Validator Node</a>
                             </div>
                             <div className="column is-6">
-                                <button className="button is-fullwidth has-text-light-purple py-5 download-btn">CF Bridge Polygon Validator Node <br/> Coming Soon</button>
+                                <button className="button is-fullwidth has-text-light-purple py-5 download-btn" disabled>CF Bridge Polygon Validator Node Coming Soon</button>
                             </div>
                         </div>
 
