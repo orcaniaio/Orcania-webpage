3374fab6cadef2ba4fe93bde36c857d1b2154028
Crypto Family <81621551+CryptoFamily3@users.noreply.github.com>
2022-01-15 00:51:59 +0200

    Update tokenomics-data.js

--- src/components/routes/token/tokenomics-data.js
+++ src/components/routes/token/tokenomics-data.js
@@ -1,20 +1,20 @@
 const data = [
     {
-        title: 'Public sale',
-        body: 'The ICO will be held after the nodes presale has been concluded. </br></br>' +
-        'Any CFC remaining from the nodes presale will be added to the public ICO, it will allow everyone to aquire CFC on multiple blockchains. <br/></br>' +
-        'The ICO will either be held on DAPPs on the supported blockchains of the Crypto Family Bridge, giving everyone a chance to participate without any limitations or requirements. <br/><br/>' +
-        'Any CFC left from the ICO will be utilized in securing the Crypto Family Bridge and will be equally delegated to across all validator nodes operating the Bridge.',
-        percentage: 40
+        title: 'Nodes Sale Parallel',
+        body: 'This allocation of CFC is to provide liquidity to the funds raised from the nodes sale. </br></br>' +
+        'Funds raised from the nodes sale will be locked as liquidity on the CF-DEX. This allocation is to provide the CFC part of the liquidity. <br/></br>' +
+        'Any CFC left from this allocation and from the nodes sale will either be utilized in securing the Crypto Family Bridge through delegating equally across all nodes, or burnt. <br/><br/>',
+        percentage: 35
 
     }, {
         title: 'Nodes sale',
         body: 'The nodes sale is to help early node operators aquire CFC to run validator nodes and secure the Crypto Family Bridge on mainnet release. <br/><br/>' +
-        'This sale will allow nodes to aquire CFC for 15% less than the IDO price to help increase the power of CFC securing the Crypto Family Bridge on release. <br/><br/>' +
-        'The nodes sale is intended to help validator node operators secure thr bridge, and as such certain terms are set to make sure CFC purchased from the nodes sale is used for its intended purpose. <br/><br/>' + 
-        'Users that aquire CFC from the nodes sale but does not use it to run a validator node will have a lock period of 2 years on their CFC. <br/>' + 
+        'This sale will allow nodes to aquire CFC at a fixed price without any fluctuations. The price will be decided based on the price of CFC on the CF-DEX<br/><br/>' +
+        'The nodes sale is intended to help validator node operators secure the bridge, and as such certain terms are set to make sure CFC purchased from the nodes sale is used for its intended purpose. <br/><br/>' + 
+        'Users that aquire CFC from the nodes sale but does not use it to run a validator node or delegate to one, will have a lock period of 2 years on their CFC. <br/>' + 
         'Nodes that aquire CFC from the nodes sale but does not put it all into running a validator node and securing the bridge, will have a lock period of 2 years on their CFC. <br/>' +
-        'Nodes that aquire CFC from the nodes sale and put it all into running a node and securing the bridge will have a lock period of 1 year on their CFC.',
+        'Nodes that aquire CFC from the nodes sale and put it all into running a node and securing the bridge will have a lock period of only 1 year on their CFC.' +
+        'After the lock period is over, users will be able to withdraw their CFC over the course of 1 year, where they can withdraw 1/12 of their bought CFC each months.',
         percentage: 35
     }, {
         title: 'Protocol development',
@@ -24,9 +24,11 @@ const data = [
 
     }, {
         title: 'Ecosystem development',
-        body: 'After the mainnet release of the Crypto Family Bridge, the Crypto Family will be contributing to the growth of the ecosystem through grants, bounties and hackathons to encourage and support developers on expanding their ideas beyond one blockchain and building multichain apps.</br></br>' +
-        '5% of the CFC release supply will be allocated to help fund such projects and push them forward on their journey, live development support from our team to the teams of these projects will be available to help them better grasp the architecture of the bridge and how to interact with it.</br></br>',
-        percentage: 5
+        body: 'After the mainnet release of the Crypto Family Bridge and Crypto Family DEX, the Crypto Family will be contributing to the growth of the ecosystem of both. <br/><br/>' + 
+        'This allocation will be used to supply CFC liquidity to the DEX, while the Crypto Family team and our partners provide other tokens to be provided as liquidity to the DEX. <br/><br/>' + 
+        'After the mainnet release of the Crypto Family Bridge, this allocation will be used to contribute to the growth of the ecosystem through grants, bounties and hackathons to encourage and support developers on expanding their ideas beyond one blockchain and building multichain apps.</br></br>' +
+        '10% of the CFC release supply will be allocated to help fund such projects and push them forward on their journey, live development support from our team to teams building on these projects will be available to help them better grasp the architecture of the DEX and the bridge and how to interact with them.</br></br>',
+        percentage: 10
     }, {
         title: 'Crypto family node',
         body: 'The Crypto Family is gonna be running the first nodes of the Crypto Family Bridge, and to ensure a secure launch we have allocated 2.5% of the CFC release supply to these nodes.</br></br>'+
